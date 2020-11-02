fd29146e32cccfeee79d74534a10a9c5a83d718a
honzza <honzzadvorak@gmail.com>
2020-11-02 23:03:59 +0100

    footer improvements

--- components/Footer.js
+++ components/Footer.js
@@ -1,49 +1,27 @@
-import Link from "next/link";
-
 export default function Footer() {
   return (
-    <footer className="footer bg-light py-5">
+    <footer className="footer bg-light py-3">
       <style jsx>{`
+        .footer {
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+        }
+
         .footer .social a {
           margin: 0 10px;
         }
       `}</style>
-
-      <div className="container grid grid-3">
-        <div>
-          <h1>honzza dvorak.</h1>
-          <p>copyright &copy; 2020</p>
-        </div>
-        <nav>
-          <ul>
-            <li>
-              <Link href="/">
-                <a>home</a>
-              </Link>
-            </li>
-            <li>
-              <Link href="/portfolio">
-                <a>portfolio</a>
-              </Link>
-            </li>
-            <li>
-              <Link href="/contact">
-                <a>contact</a>
-              </Link>
-            </li>
-          </ul>
-        </nav>
-        <div className="social">
-          <a href="#">
-            <i className="fab fa-github fa-2x"></i>
-          </a>
-          <a href="#">
-            <i className="fab fa-twitter fa-2x"></i>
-          </a>
-          <a href="#">
-            <i className="fab fa-facebook fa-2x"></i>
-          </a>
-        </div>
+      <div>
+        <p>webdesign honzza dvorak 2020</p>
+      </div>
+      <div className="social">
+        <a href="#">
+          <i className="fab fa-github fa-2x"></i>
+        </a>
+        <a href="#">
+          <i className="fas fa-envelope fa-2x"></i>
+        </a>
       </div>
     </footer>
   );
