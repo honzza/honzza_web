b9975b578d5478d080fc2d9be93358c5683c1634
honzza <honzzadvorak@gmail.com>
2020-11-06 23:11:38 +0100

    landing page initial

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,25 @@
 import Layout from "../components/Layout";
-import { Text } from "@chakra-ui/core";
+import { Stack, Box, Heading, Flex } from "@chakra-ui/core";
+
+function StackEx() {
+  return (
+    <Stack spacing={8} my="30px" mx="20px">
+      <Box p={5} shadow="md" borderWidth="1px">
+        <Heading fontSize="xl">Languages</Heading>
+        <Flex>
+          <i class="fab fa-html5 fa-5x"></i>
+          <i class="fab fa-python fa-5x"></i>
+          <i class="fab fa-css3-alt fa-5x"></i>
+        </Flex>
+      </Box>
+    </Stack>
+  );
+}
 
 export default function Index() {
   return (
     <Layout>
-      <Text my='10px'>home alone</Text>
+      <StackEx />
     </Layout>
   );
 }
