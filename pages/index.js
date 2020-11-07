bc405b1e942a71f74da2e3f052c6abfdeaae8615
honzza <honzzadvorak@gmail.com>
2020-11-07 22:46:26 +0100

    index page content

--- pages/index.js
+++ pages/index.js
@@ -3,13 +3,34 @@ import { Stack, Box, Heading, Flex } from "@chakra-ui/core";
 
 function StackEx() {
   return (
-    <Stack spacing={8} my="30px" mx="20px">
+    <Stack isInline spacing={8} px="40px" my="30px" mx="auto" maxWidth="1100px">
       <Box p={5} shadow="md" borderWidth="1px">
-        <Heading fontSize="xl">Languages</Heading>
+        <Heading fontSize="xl" pb="10px">
+          Languages
+        </Heading>
         <Flex>
-          <i class="fab fa-html5 fa-5x"></i>
-          <i class="fab fa-python fa-5x"></i>
-          <i class="fab fa-css3-alt fa-5x"></i>
+          <Flex direction="column" align="center" mr="20px">
+            <i class="fab fa-html5 fa-5x"></i>html 5
+          </Flex>
+          <Flex direction="column" align="center" mr="20px">
+            <i class="fab fa-css3-alt fa-5x"></i>css 3
+          </Flex>
+          <Flex direction="column" align="center" mr="20px">
+            <i class="fab fa-js fa-5x"></i>javascript
+          </Flex>
+          <Flex direction="column" align="center">
+            <i class="fab fa-python fa-5x"></i>python
+          </Flex>
+        </Flex>
+      </Box>
+      <Box p={5} shadow="md" borderWidth="1px">
+        <Heading fontSize="xl" pb="10px">
+          Frameworks
+        </Heading>
+        <Flex>
+          <Flex direction="column" align="center" mr="20px">
+            <i class="fab fa-react fa-5x"></i>react
+          </Flex>
         </Flex>
       </Box>
     </Stack>
